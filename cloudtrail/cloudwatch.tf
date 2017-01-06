ffe4e99fb040bf60caa37cfa58a794b09cb18624
Daniel Adams <danieladams456@gmail.com>
2017-01-05 23:08:19 -0500

    figured policy to allow cloudwatch publish to sns

--- cloudtrail/cloudwatch.tf
+++ cloudtrail/cloudwatch.tf
@@ -26,6 +26,49 @@ PATTERN
 
 resource "aws_sns_topic" "root_login" {
   name = "root_login_notification"
+  #policy example from below url
+  #http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/resource-based-policies-cwe.html#sns-permissions
+  policy = <<POLICYEOF
+{
+  "Version": "2008-10-17",
+  "Id": "__default_policy_ID",
+  "Statement": [
+    {
+      "Sid": "AllowOwnerFullControl",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "SNS:Publish",
+        "SNS:RemovePermission",
+        "SNS:SetTopicAttributes",
+        "SNS:DeleteTopic",
+        "SNS:ListSubscriptionsByTopic",
+        "SNS:GetTopicAttributes",
+        "SNS:Receive",
+        "SNS:AddPermission",
+        "SNS:Subscribe"
+      ],
+      "Resource": "arn:aws:sns:us-east-1:225730437332:root_login_notification",
+      "Condition": {
+        "StringEquals": {
+          "AWS:SourceOwner": "225730437332"
+        }
+      }
+    },
+    {
+      "Sid": "TrustCWEToPublishEventsToRootLoginNotification",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sns:Publish",
+      "Resource": "arn:aws:sns:us-east-1:225730437332:root_login_notification"
+    }
+  ]
+}
+POLICYEOF
 }
 
 resource "aws_cloudwatch_event_target" "root_login_sns" {
