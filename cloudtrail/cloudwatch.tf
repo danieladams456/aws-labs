6690982d63bce75b7cfcc6bea9684db903a4c814
Daniel Adams <danieladams456@gmail.com>
2017-01-05 22:46:54 -0500

    almost there with alerts, need to figure out what policy the sns topic needs

--- cloudtrail/cloudwatch.tf
+++ cloudtrail/cloudwatch.tf
@@ -3,3 +3,36 @@ resource "aws_cloudwatch_log_group" "cloudtrail_logs" {
   name = "cloudtrail_logs"
   retention_in_days = 90
 }
+
+#event rule for email
+resource "aws_cloudwatch_event_rule" "root_login" {
+  name = "root_login_notification"
+  description = "Send an email when root logs in."
+  event_pattern = <<PATTERN
+{
+  "detail-type": [
+    "AWS Console Sign In via CloudTrail"
+  ],
+  "detail": {
+    "userIdentity": {
+      "arn": [
+        "arn:aws:iam::225730437332:root"
+      ]
+    }
+  }
+}
+PATTERN
+}
+
+resource "aws_sns_topic" "root_login" {
+  name = "root_login_notification"
+}
+
+resource "aws_cloudwatch_event_target" "root_login_sns" {
+  rule = "${aws_cloudwatch_event_rule.root_login.name}"
+  arn = "${aws_sns_topic.root_login.arn}"
+  target_id = "root_login_sns"
+  input = <<EOF
+{"Message": "The root account has logged in."}
+EOF
+}
